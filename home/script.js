73e3fb355a525932b43df7c62cc8e724bb865f74
JcAPC <jaycemarkpresbitero1902@gmail.com>
2025-03-06 21:38:08 +0800

    up

--- home/script.js
+++ home/script.js
@@ -8,12 +8,10 @@ document.addEventListener("DOMContentLoaded", () => {
             tooltip.textContent = item.getAttribute("data-tooltip");
             document.body.appendChild(tooltip);
 
-            requestAnimationFrame(() => {
-                const rect = item.getBoundingClientRect();
-                tooltip.style.left = `${rect.left + rect.width / 2 - tooltip.offsetWidth / 2}px`;
-                tooltip.style.top = `${rect.top - tooltip.offsetHeight - 10}px`;
-                tooltip.style.display = "block";
-            });
+            const rect = e.target.getBoundingClientRect();
+            tooltip.style.left = `${rect.left + rect.width / 2 - tooltip.clientWidth / 2}px`;
+            tooltip.style.top = `${rect.top - tooltip.clientHeight - 10}px`;
+            tooltip.style.display = "block";
 
             item.addEventListener("mouseleave", () => {
                 tooltip.remove();
