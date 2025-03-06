b98254d834e9942e96f909f6d20e4c5adf7fe14d
JcAPC <jaycemarkpresbitero1902@gmail.com>
2025-03-06 20:08:13 +0800

    uP

---
+++ home/script.js
@@ -0,0 +1,11 @@
+const roadmapItems = document.querySelectorAll('.roadmap-item');
+
+roadmapItems.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    item.style.transform = 'scale(1.1)';
+  });
+
+  item.addEventListener('mouseout', () => {
+    item.style.transform = 'scale(1)';
+  });
+});
